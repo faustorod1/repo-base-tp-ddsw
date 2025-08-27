e946c82a1d954db8c551c6571db036be40b6e174
Juan Tarducci <jtarducci@frba.utn.edu.ar>
2025-08-27 16:48:42 -0300

    Correcciones
    Cambiamos Date().now() por new Date() devuelve una instancia del tiempo ahora

--- packages/backend/src/models/entities/notificacion.js
+++ packages/backend/src/models/entities/notificacion.js
@@ -18,13 +18,13 @@ class CambioEstadoPedido {
         this.id = id;
         this.usuarioDestino = usuarioDestino;
         this.mensaje = mensaje;
-        this.fechaAlta = new Date.now();
+        this.fechaAlta = new Date();
         this.leida = false;
         this.fechaLeida = null;
     }
 
     marcarComoLeida(){
-        this.leida = true
-        this.fechaLeida = Date.now()
+        this.leida = true;
+        this.fechaLeida = new Date();
     }
 }
